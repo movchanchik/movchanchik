4cd6148a6b195194070c6796f121d02bdb6e50c8
movchanchik <inna0992@gmail.com>
2025-10-19 17:36:01 -0400

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <div className="pageWrapper">
           <SpeedInsights />
+          <Analytics />
           <Header />
           <main className="container">{children}</main>
           <Footer />
