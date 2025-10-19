3072d1e79cfbbd01ec0137e9fd8900b7b0a97682
movchanchik <inna0992@gmail.com>
2025-10-19 16:21:42 -0400

    fix

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,7 @@
-"use client";
 import React from "react";
 import UnderConstruction from "../components/underConstruction/UnderConstruction";
 
 function Blog() {
-  const response = await fetch("https://api.linkedin.com/v2/me", {
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
-  });
-  const profile = await response.json();
-  console.log(profile);
-
   return <UnderConstruction pageTitle="Blog" />;
 }
 
