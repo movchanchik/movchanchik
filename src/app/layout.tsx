fdb102f70ce5cb19cdcc401ee1a0f51bcbd50247
movchanchik <inna0992@gmail.com>
2025-10-19 17:39:46 -0400

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +27,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <div className="pageWrapper">
-          <SpeedInsights />
-          <Analytics />
           <Header />
           <main className="container">{children}</main>
           <Footer />
